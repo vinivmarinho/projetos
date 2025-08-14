f3c0de5167814096a277c6287dbc48cab0a8d563
Vinícius Marinho <vini.marinho2004@gmail.com>
2025-08-14 10:07:33 -0400

    Corrigir o bug da função

--- desafio_05/script/script.js
+++ desafio_05/script/script.js
@@ -8,18 +8,18 @@ function teste_gratis() {
     if (nome.value === "") {
         nome.classList.add("placeholder_vermelho")
         nome.placeholder = "Por favor, preencha o campo"
-        if (sobrenome.value === "") {
-            sobrenome.classList.add("placeholder_vermelho")
-            sobrenome.placeholder = "Por favor, preencha o campo"
-        }
-        if (email.value === ""){
-            email.classList.add("placeholder_vermelho")
-            email.placeholder = "Por favor, preencha o campo"
-        }
-        if (senha.value === "") {
-            senha.placeholder = "Por favor, preencha o campo"
-            senha.classList.add("placeholder_vermelho")
-        }
+    }
+    if (sobrenome.value === "") {
+        sobrenome.classList.add("placeholder_vermelho")
+        sobrenome.placeholder = "Por favor, preencha o campo"
+    }
+    if (email.value === ""){
+        email.classList.add("placeholder_vermelho")
+        email.placeholder = "Por favor, preencha o campo"
+    }
+    if (senha.value === "") {
+        senha.placeholder = "Por favor, preencha o campo"
+        senha.classList.add("placeholder_vermelho")
     }
 }
 botao.addEventListener("click", teste_gratis)

ded70851455e9dd1759d4df16efe219d70db22ae
Vinícius Marinho <vini.marinho2004@gmail.com>
2025-08-14 10:28:42 -0400

    Atualizei a função de validação

--- desafio_05/script/script.js
+++ desafio_05/script/script.js
@@ -4,22 +4,20 @@ let sobrenome = document.querySelector("input#id_sobrenome")
 let email = document.querySelector("input#id_email")
 let senha = document.querySelector("input#id_senha")
 
-function teste_gratis() {
-    if (nome.value === "") {
-        nome.classList.add("placeholder_vermelho")
-        nome.placeholder = "Por favor, preencha o campo"
-    }
-    if (sobrenome.value === "") {
-        sobrenome.classList.add("placeholder_vermelho")
-        sobrenome.placeholder = "Por favor, preencha o campo"
-    }
-    if (email.value === ""){
-        email.classList.add("placeholder_vermelho")
-        email.placeholder = "Por favor, preencha o campo"
-    }
-    if (senha.value === "") {
-        senha.placeholder = "Por favor, preencha o campo"
-        senha.classList.add("placeholder_vermelho")
+// Função que valida qualquer input //
+function validacao(input) {
+    if (input.value === "") {
+        input.classList.add("placeholder_vermelho")
+        input.placeholder = "Por favor, preencha o campo"
+    } else {
+        input.classList.remove("placeholder_vermelho")
     }
 }
-botao.addEventListener("click", teste_gratis)
+// Chama a função "validacao" pra cada input
+function valida_campos() {
+    validacao(nome)
+    validacao(sobrenome)
+    validacao(email)
+    validacao(senha)
+}
+botao.addEventListener("click", valida_campos)

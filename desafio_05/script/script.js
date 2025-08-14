eb3e6f4a3689d6ca6f8df3445a8cf306eb3c05e4
Vinícius Marinho <vini.marinho2004@gmail.com>
2025-08-14 09:54:05 -0400

    Testando função de validação de campo

--- desafio_05/script/script.js
+++ desafio_05/script/script.js
@@ -1,11 +1,25 @@
 let botao = document.getElementById("id_teste_gratis")
 let nome = document.querySelector("input#id_nome")
+let sobrenome = document.querySelector("input#id_sobrenome")
+let email = document.querySelector("input#id_email")
+let senha = document.querySelector("input#id_senha")
+
 function teste_gratis() {
-    if (nome.value.toLowerCase() === "isabelly" || nome.value.toLowerCase() === "isa") {
-        alert("Oi, meu amor! Que saudade!")
-    } else {
-        alert(`Olá, ${nome.value}! tudo bem?`)
+    if (nome.value === "") {
+        nome.classList.add("placeholder_vermelho")
+        nome.placeholder = "Por favor, preencha o campo"
+        if (sobrenome.value === "") {
+            sobrenome.classList.add("placeholder_vermelho")
+            sobrenome.placeholder = "Por favor, preencha o campo"
+        }
+        if (email.value === ""){
+            email.classList.add("placeholder_vermelho")
+            email.placeholder = "Por favor, preencha o campo"
+        }
+        if (senha.value === "") {
+            senha.placeholder = "Por favor, preencha o campo"
+            senha.classList.add("placeholder_vermelho")
+        }
     }
-    
 }
 botao.addEventListener("click", teste_gratis)

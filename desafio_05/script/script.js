e646812a49baca98003ce93a8cf062eedf27732b
Vinícius Marinho <vini.marinho2004@gmail.com>
2025-08-18 09:14:11 -0400

    Criei a função que valida a quantidade de caracteres dos campos

--- desafio_05/script/script.js
+++ desafio_05/script/script.js
@@ -3,8 +3,21 @@ let nome = document.querySelector("input#id_nome")
 let sobrenome = document.querySelector("input#id_sobrenome")
 let email = document.querySelector("input#id_email")
 let senha = document.querySelector("input#id_senha")
-
-// Função que valida qualquer input //
+//Função que valida o a quantidade de caracteres
+function valida_tamanho(id, tamanhoMinimo) {
+    let campo = document.getElementById(id) // Campo recebe o id que será dado ao chamar a função
+    // Obs: "campo" se refere ao elemento Html(input). O "id" se refere ao id do elemento Html(input)
+    if(campo.value.length < tamanhoMinimo) {
+        alert(`O campo "${campo.placeholder}" deve ter pelo menos ${tamanhoMinimo} caracteres`)
+        campo.focus() // Foca o mouse no campo correspondente
+        campo.classList.add("placeholder_vermelho")
+        return false // Validação falhou
+    } else{
+        campo.classList.remove("placeholder_vermelho")
+        return true // Validação concluída
+    }
+}
+// Função que valida se o input estiver vazio //
 function validacao(input) {
     if (input.value === "") {
         input.classList.add("placeholder_vermelho")
@@ -16,8 +29,11 @@ function validacao(input) {
 // Chama a função "validacao" pra cada input
 function valida_campos() {
     validacao(nome)
+    valida_tamanho("id_nome", 3)
     validacao(sobrenome)
+    valida_tamanho("id_sobrenome", 4)
     validacao(email)
     validacao(senha)
+    valida_tamanho("id_senha", 8)
 }
 botao.addEventListener("click", valida_campos)

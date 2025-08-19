1e3a0656933c31ac98fe6db5ede06ef495fb85f8
Vinícius Marinho <vini.marinho2004@gmail.com>
2025-08-19 08:57:11 -0400

    Atualizei a validação dos campos

--- desafio_05/script/script.js
+++ desafio_05/script/script.js
@@ -20,21 +20,25 @@ function valida_tamanho(id, tamanhoMinimo) {
 // Função que valida se o input estiver vazio //
 function validacao(input) {
     if (input.value === "") {
+        input.classList.remove("input") 
         input.classList.add("placeholder_vermelho")
         input.placeholder = "Por favor, preencha o campo"
+        return false
     } else {
         input.classList.remove("placeholder_vermelho")
+        return true
     }
 }
 // Chama as funções "validacao" e "valida_tamanho" pra cada input
-function valida_campos() {
-    validacao(nome)
-    valida_tamanho("id_nome", 3)
-    validacao(sobrenome)
-    valida_tamanho("id_sobrenome", 4)
-    validacao(email)
-    valida_tamanho("id_email", 6)
-    validacao(senha)
-    valida_tamanho("id_senha", 8)
+function valida_campos() { 
+    // Se a função "validação" retornar true, faz a função "valida_tamanho"
+    if (validacao(nome))
+        valida_tamanho("id_nome", 3)
+    if(validacao(sobrenome))
+        valida_tamanho("id_sobrenome", 4)
+    if (validacao(email))
+        valida_tamanho("id_email", 6)
+    if (validacao(senha))
+        valida_tamanho("id_senha", 8)
 }
 botao.addEventListener("click", valida_campos)

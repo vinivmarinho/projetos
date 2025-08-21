add7962a74104db0ef16ee470c302b354e5e5afb
Vinícius Marinho <vini.marinho2004@gmail.com>
2025-08-21 09:51:59 -0400

    Adicionei o efeito de hover usando JavaScript

--- etch_a_sketch/script.js
+++ etch_a_sketch/script.js
@@ -3,6 +3,14 @@ for (let inicio = 1; inicio <= 256; inicio++) {
     let quadrado = document.createElement("div")
     quadrado.classList.add("quadrado")
     container.appendChild(quadrado)
+
+    quadrado.addEventListener("mouseenter", () => {
+        // Quando o mouse estiver em cima do quadrado, ele recebe a classe "quadrado_ativo"
+        quadrado.classList.add("quadrado_ativo")
+    })
+    quadrado.addEventListener("mouseleave", () => {
+        quadrado.classList.remove("quadrado_ativo")
+    })
 }
 
 

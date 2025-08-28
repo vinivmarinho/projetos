f73ff76a81725a4442270dc5517862545c49c7aa
Vinícius Marinho <vini.marinho2004@gmail.com>
2025-08-28 10:54:51 -0400

    Fiz as funções dos botões de deletar e limpar

--- calculadora/script.js
+++ calculadora/script.js
@@ -1,3 +1,20 @@
+let display = document.querySelector("div#input_usuario")
+let digitos = "" // String que guarda os dígitos digitados
+function atualiza_display(botao) {
+    digitos += botao
+    display.textContent = digitos
+}
+function limpar() {
+    digitos = "" // Reseta os números
+    display.textContent = "0" // Mostra 0 no display
+}
+function apagar_ultimo() {
+    digitos = digitos.slice(0, -1) // Apaga o último dígito de "numeros"
+    display.textContent = digitos // mostra "numeros" no display
+    if (display.textContent === "") { // Se o display terminar vazio, ele recebe "0"
+        display.textContent = "0"
+    }
+}
 function soma(n1, n2) {
     let soma = n1 + n2
     return soma;

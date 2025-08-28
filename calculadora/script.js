9ff31585212fdd3e91c45d4a77ac6cc0ec5a635d
Vinícius Marinho <vini.marinho2004@gmail.com>
2025-08-28 14:11:23 -0400

    Comentei o código. Os números digitados aparecem em outra div agora

--- calculadora/script.js
+++ calculadora/script.js
@@ -1,20 +1,68 @@
 let display = document.querySelector("div#input_usuario")
+let resultado = document.querySelector("div#resultado")
 let digitos = "" // String que guarda os dígitos digitados
 function atualiza_display(botao) {
     digitos += botao
-    display.textContent = digitos
+    resultado.textContent = digitos
 }
-function limpar() {
+function limpar() { // Botão "C" chama essa função
     digitos = "" // Reseta os números
-    display.textContent = "0" // Mostra 0 no display
+    resultado.textContent = "0" // Mostra 0 no display
 }
-function apagar_ultimo() {
+function apagar_ultimo() { // Botão "Del" chama essa função
     digitos = digitos.slice(0, -1) // Apaga o último dígito de "numeros"
-    display.textContent = digitos // mostra "numeros" no display
-    if (display.textContent === "") { // Se o display terminar vazio, ele recebe "0"
-        display.textContent = "0"
+    resultado.textContent = digitos // mostra "numeros" no display
+    if (resultado.textContent === "") { // Se o display terminar vazio, ele recebe "0"
+        resultado.textContent = "0"
     }
 }
+function soma(n1, n2) {
+    let soma = n1 + n2
+    return soma;
+}
+function subtracao(n1, n2) {
+    let subtracao = n1 - n2
+    return subtracao
+}
+
+function multiplicacao(n1, n2) {
+    let multi = n1 * n2
+    return multi
+}
+
+function divisao(n1, n2) {
+    
+    if (n2 === 0) {
+        alert("Não é possível dividir por zero")
+        return null
+    } else {
+        let divisao = n1 / n2
+        return divisao
+    }
+    
+}
+// Função que recebe o operador e dependendo do que receber, vai chamar uma das funções já criadas
+function operacao(operador, n1, n2) {
+    if (operador === "+") {
+        return soma(n1, n2)
+    } else if (operador === "-") {
+        return subtracao(n1, n2)
+    } else if (operador === "*") {
+        return multiplicacao(n1, n2)
+    } else if (operador === "/") {
+        return divisao(n1, n2)
+    }
+}
+
+
+
+
+
+
+
+
+
+
 function soma(n1, n2) {
     let soma = n1 + n2
     return soma;

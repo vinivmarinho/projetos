316790f9000c6ec164f13d21edb3bf16b9d260e1
Vinícius Marinho <vini.marinho2004@gmail.com>
2025-08-29 11:45:21 -0400

    Fiz a função de operação, mas só com o sinal de soma

--- calculadora/script.js
+++ calculadora/script.js
@@ -2,31 +2,38 @@ let display = document.querySelector("div#input_usuario")
 let resultado = document.querySelector("div#resultado")
 let numeros = document.querySelectorAll("button.numero") // Todos os botões."numero"
 let operadores = document.querySelectorAll("button.operador") // Todos os botões."operador"
-let digitos = ""
+let digitos = "" // Guarda toda a expressão
+let igual = document.getElementById("igual")
+let sinal = "" // Recebe o operador
+let n1 = ""
+let n2 = ""
+
 numeros.forEach((botao) => { // Passa por todos botões dentro de "numeros"
     // Adiciona um evento "click" que chama a função "adiciona_numero"
     botao.addEventListener("click", () => adiciona_numero(botao.textContent))
-})
+}) // Ok
 
-function adiciona_numero(numero) {
+function adiciona_numero(numero) { 
     digitos += numero
     resultado.textContent = digitos
-}
+} // Ok
 
 operadores.forEach((botao) => {
     botao.addEventListener("click", () => adiciona_operador(botao.textContent))
-})
+}) // Ok
 
 function adiciona_operador(operador) {
     digitos += operador
     resultado.textContent = digitos
-}
+    sinal = operador
+} // Ok
 
 
 function limpar() { // Botão "C" chama essa função
     digitos = "" // Reseta os números
     resultado.textContent = "0" // Mostra 0 no display
-}
+    display.textContent = ""
+} // Ok
 
 function apagar_ultimo() { // Botão "Del" chama essa função
     digitos = digitos.slice(0, -1) // Apaga o último dígito de "numeros"
@@ -35,25 +42,29 @@ function apagar_ultimo() { // Botão "Del" chama essa função
         resultado.textContent = "0"
     }
 }
+igual.addEventListener("click", () => operacao(sinal, n1, n2)) // O botão "igual" chama a função "operacao" quando é clicado
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// Função que recebe o operador e dependendo do operador, vai chamar uma das funções
+function operacao(operador, n1, n2) {
+    // "indexOf" encontra o índice da primeira ocorrência de um caractere
+    let posicao = digitos.indexOf(operador)
+    // Substring => extrai uma parte da string original, retornando uma nova string
+    n1 = parseFloat(digitos.substring(0, posicao)) // 0 => Do primeiro índice até a "posicao" (posicao do operador)
+    n2 = parseFloat(digitos.substring(posicao + 1)) // posicao => Posição do operador até o final da string(é isso o que "+1" representa)
+    if (operador === "+") {
+        display.textContent = n1
+        display.textContent += operador
+        display.textContent += n2
+        resultado.textContent = soma(n1, n2)
+    } else if (operador === "-") {
+        return subtracao(n1, n2)
+    } else if (operador === "*") {
+        return multiplicacao(n1, n2)
+    } else if (operador === "/") {
+        return divisao(n1, n2)
+    }
+}
 
 
 
@@ -82,62 +93,9 @@ function divisao(n1, n2) {
     }
     
 }
-// Função que recebe o operador e dependendo do que receber, vai chamar uma das funções já criadas
-function operacao(operador, n1, n2) {
-    if (operador === "+") {
-        return soma(n1, n2)
-    } else if (operador === "-") {
-        return subtracao(n1, n2)
-    } else if (operador === "*") {
-        return multiplicacao(n1, n2)
-    } else if (operador === "/") {
-        return divisao(n1, n2)
-    }
-}
 
 
 
 
 
 
-
-
-
-
-function soma(n1, n2) {
-    let soma = n1 + n2
-    return soma;
-}
-function subtracao(n1, n2) {
-    let subtracao = n1 - n2
-    return subtracao
-}
-
-function multiplicacao(n1, n2) {
-    let multi = n1 * n2
-    return multi
-}
-
-function divisao(n1, n2) {
-    
-    if (n2 === 0) {
-        alert("Não é possível dividir por zero")
-        return null
-    } else {
-        let divisao = n1 / n2
-        return divisao
-    }
-    
-}
-// Função que recebe o operador e dependendo do que receber, vai chamar uma das funções já criadas
-function operacao(operador, n1, n2) {
-    if (operador === "+") {
-        return soma(n1, n2)
-    } else if (operador === "-") {
-        return subtracao(n1, n2)
-    } else if (operador === "*") {
-        return multiplicacao(n1, n2)
-    } else if (operador === "/") {
-        return divisao(n1, n2)
-    }
-}

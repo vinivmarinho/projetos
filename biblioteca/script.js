01f2ff8b93cf23dc73940f64fbdaabb64029a695
Vinícius Marinho <vini.marinho2004@gmail.com>
2025-09-08 09:12:32 -0400

    Criei função que adiciona o objeto "livro" no array "livraria"

--- biblioteca/script.js
+++ biblioteca/script.js
@@ -6,8 +6,16 @@ function cria_livro(titulo, autor, ano) {
     this.titulo = titulo
     this.autor = autor
     this.ano = ano
+    this.id = crypto.randomUUID() // Gera um id único
+    console.log("Livro criado com sucesso!")
+    console.log(`Título: ${this.titulo}, autor: ${this.autor}, ano: ${this.ano}, Id: ${this.id}`)
 }
-
-function adiciona_livro() {
-    // Adiciona o livro à "livraria"
+function adiciona_livro(livro) {
+   // Adiciona o livro à "livraria"
+   livraria.push(livro) // Array adiciona o objeto "Livro"
+   console.log(`${livro["titulo"]} adicionado com sucesso`)
 }
+livro1 = new cria_livro("Harry Potter", "Fulano", 2025)
+livro2 = new cria_livro("Percy Jackson", "Ciclano", 2000)
+adiciona_livro(livro1)
+adiciona_livro(livro2)

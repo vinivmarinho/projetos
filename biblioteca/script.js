0c9399eccc5f97c1ff78dde548b7a91e96215040
Vinícius Marinho <vini.marinho2004@gmail.com>
2025-09-08 08:49:55 -0400

    Criando array para armazenar os livros e uma função construtora para criar objetos do tipo "Livro"

---
+++ biblioteca/script.js
@@ -0,0 +1,13 @@
+// Array de livros
+const livraria = []
+
+function cria_livro(titulo, autor, ano) {
+    // Construtor do livro
+    this.titulo = titulo
+    this.autor = autor
+    this.ano = ano
+}
+
+function adiciona_livro() {
+    // Adiciona o livro à "livraria"
+}

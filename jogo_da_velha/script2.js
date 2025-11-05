1ecbdb8b3aa99cb634cc859ac405dcbe7cfcf98e
vinivmarinho <vini.marinho2004@gmail.com>
2025-11-05 19:40:25 -0400

    Feat: Cria função de fábrica, o módulo e testa o funcionamento da função de marcar na página web

--- jogo_da_velha/script2.js
+++ jogo_da_velha/script2.js
@@ -1,14 +1,36 @@
-// Criando as variáveis correspondentes as divs do tabuleiro
-let posicoes = document.querySelectorAll(".bloco") // Todos os blocos do tabuleiro
+// Criando as variáveis correspondentes as divs do tabuleiro 
 let comecar = document.getElementById("comecar") // Botão de start
+moduloJogoDaVelha = (function() {
+    let posicoes = document.querySelectorAll(".bloco") // Todos os blocos do tabuleiro
+
+    // Função de fábrica que cria objetos de jogadores
+    function criaJogador(nome, simbolo) {
+        // Função que marca o símbolo no tabuleiro
+        marcar = function(bloco) { // Passa o bloco que foi clicado
+            bloco.innerHTML = simbolo // Adiciona o símbolo no bloco pressionado
+        }
+        return {marcar, nome, simbolo}
+    }
+
+    return {criaJogador, posicoes}
+})()
+
+
+
+
+let jogador1 = moduloJogoDaVelha.criaJogador("Vini", "X")
+
 
 comecar.addEventListener("click", function() {
-    alert(posicoes[0].innerHTML)
+    alert(moduloJogoDaVelha.posicoes[0].innerHTML)
+    
 })
 
 // Teste para adicionar um evento pra cada bloco que for clicado
-posicoes.forEach(bloco => {
+moduloJogoDaVelha.posicoes.forEach(bloco => {
     bloco.addEventListener("click", () => {
-        alert(`Você clicou na posição: ${bloco.innerHTML}`)
+        // Testando usar a função "marcar" que eu criei
+        jogador1.marcar(bloco)
+        // alert(`Você clicou na posição: ${bloco.innerHTML}`)
     })
 })

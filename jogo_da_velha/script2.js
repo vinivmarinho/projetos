409c0fe542454343c03be093f5de102ff6c3056b
Vinícius Marinho <vini.marinho2004@gmail.com>
2025-11-11 09:47:24 -0300

    feat: Adiciona verificação se os espaços estão vazios antes de marcar

--- jogo_da_velha/script2.js
+++ jogo_da_velha/script2.js
@@ -4,7 +4,12 @@ let comecar = document.getElementById("comecar") // Botão de start
 // Usando o module pattern para deixar variáveis e funções privadas, exceto as que forem retornadas
 moduloJogoDaVelha = (function() {
     let posicoes = document.querySelectorAll(".bloco") // Todos os blocos do tabuleiro. Obs: é uma node List (Uma lista de nós do DOM)
-
+    // Lista que contém as sequências de vitória
+    let listaGeral = [
+        [posicoes[0], posicoes[1], posicoes[2]],
+        [posicoes[3], posicoes[4], posicoes[5]],
+        [posicoes[6], posicoes[7], posicoes[8]],
+    ]
     // Função de fábrica que cria objetos de jogadores
     function criaJogador(nome, simbolo) {
         // Função que marca o símbolo no tabuleiro
@@ -18,20 +23,23 @@ moduloJogoDaVelha = (function() {
         let jogadorDaVez = jogador1 // É executado uma única vez, apenas quando a função é chamada
 
         posicoes.forEach(bloco => { // Para cada "bloco" dentro de "posicoes"
-            bloco.addEventListener("click", () => { // Adiciona um evento em cada bloco
-                jogadorDaVez.marcar(bloco) // Marca o bloco
 
-                // Alternando entre os jogadores
-                if (jogadorDaVez === jogador1) {
-                    jogadorDaVez = jogador2
-                } else {
-                    jogadorDaVez = jogador1
+            bloco.addEventListener("click", () => { // Adiciona um evento em cada bloco
+                // Verificar se o espaço estiver vazio 
+                if (bloco.innerText === "") {
+                    jogadorDaVez.marcar(bloco) // Marca o bloco
+                    // Alternando entre os jogadores
+                    if (jogadorDaVez === jogador1) {
+                        jogadorDaVez = jogador2
+                    } else {
+                        jogadorDaVez = jogador1
+                    }
                 }
             })
         })
     }
 
-    return {criaJogador, iniciarJogo}
+    return {criaJogador, iniciarJogo, posicoes}
 })()
 
 
@@ -44,6 +52,7 @@ let jogador2 = moduloJogoDaVelha.criaJogador(jogador2Input.value, "O")
 
 
 comecar.addEventListener("click", moduloJogoDaVelha.iniciarJogo(jogador1, jogador2)) // Chama a função "jogar" quando o botão é clicado
+
 
 
 

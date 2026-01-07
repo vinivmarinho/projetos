3d11b3f1c31577a89f103f857fb9a3215d4b0655
Vinícius Marinho <vini.marinho2004@gmail.com>
2026-01-07 14:43:55 -0400

    feat: Adiciona Componente que permite selecionar a fonte usada na página
    Por enquanto ainda não muda a fonte da página de fato. A funcionalidade ainda será adicionada

--- dicionario/src/App.jsx
+++ dicionario/src/App.jsx
@@ -1,12 +1,14 @@
 // Arquivo que contém o componente principal "App". Aqui é escolhido qual código vai rodar
+import { useState } from "react";
 import "./style/App.css"; // Importa o estilo
-import Cabecalho from "./header.jsx"
+import Cabecalho from "./header.jsx";
 function App() {
+  const [fonte, setFonte] = useState("Arial"); // Estado que armazena a fonte atual e escolhida pelo usuário. ("fonte" => state atual. "setFonte" => Função que atualiza a fonte. "Arial" => Valor usado no primeiro render)
   return (
-    <>
-    <Cabecalho />
-    </>
-  ) 
+    <div>
+      <Cabecalho />
+    </div>
+  );
 }
 
 export default App;
--- dicionario/src/header.jsx
+++ dicionario/src/header.jsx
@@ -1,10 +1,33 @@
 // Criar componente para o cabeçalho e a barra de pesquisa
+import { useState } from "react";
 import logo from "./assets/logo.svg"; // Importa a logo
-import "./style/header.css" // Importa o arquivo de estilo
+import "./style/header.css"; // Importa o arquivo de estilo
 export default function Cabecalho() {
   return (
     <header className="cabecalho">
-      <img src={logo} alt="Teste" />
+      <Logo />
+      <SelecionaFonte />
     </header>
+  );
+}
+// Componente da logo
+function Logo() {
+  return <img src={logo} alt="Logo" />;
+}
+
+// Controla a escolha da fonte
+function SelecionaFonte({ fonte, setFonte}) {
+  const fontes = ["Arial", "Sans-serif", "Serif", "Times New Roman"]; // Lista de fontes
+  return (
+    // "select" cria o menu. O "value" é a opção que está selecionado do estado "fonte"
+    // O "onChange" faz com que quando o usuário escolha outra fonte, ela muda e o react renderiza novamente com a fonte selecionada
+    // Uso o método "map" para criar as opções do menu percorrendo a lista de fontes
+    <select value={fonte} onChange={(e) => setFonte(e.target.value)}>
+      {fontes.map((f) => (
+        <option key={f} value={f}>
+          {f}
+        </option>
+      ))}
+    </select>
   );
 }

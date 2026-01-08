b5391f6fa315b1c17a0b6cc4749f070a34ee25db
Vinícius Marinho <vini.marinho2004@gmail.com>
2026-01-08 14:33:57 -0400

    docs: Adiciona um fluxo do funcionamento do estado "fonte"

--- dicionario/src/App.jsx
+++ dicionario/src/App.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import "./style/App.css"; // Importa o estilo
 import Cabecalho from "./header.jsx";
 function App() {
-  let [fonte, setFonte] = useState("Arial"); // Estado que armazena a fonte atual e escolhida pelo usuário. ("fonte" => state atual. "setFonte" => Função que atualiza a fonte. "Arial" => Valor usado no primeiro render)
+  const [fonte, setFonte] = useState("Arial"); // Estado que armazena a fonte atual e escolhida pelo usuário. ("fonte" => state atual. "setFonte" => Função que atualiza a fonte. "Arial" => Valor usado no primeiro render)
   return (
     <div className={fonte}> {/*Div ganha a classe para que no css a font-family da página seja igual a fonte escolhida no menu "select"*/}
       <Cabecalho fonte={fonte} setFonte={setFonte} />
